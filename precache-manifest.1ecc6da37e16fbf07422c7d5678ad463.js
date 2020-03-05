ae1a0d98b447f9d47d16216e56fd3b852036af29
David Polido <dav.polido@gmail.com>
2020-03-05 10:25:14 +0000

    Updates

---
+++ precache-manifest.1ecc6da37e16fbf07422c7d5678ad463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b35b919856dd62c91a0c8dab062756",
+    "url": "/expenses-tracker/index.html"
+  },
+  {
+    "revision": "e17fa4977c04eb7f3474",
+    "url": "/expenses-tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "93b0f68d02d977ce0b2f",
+    "url": "/expenses-tracker/static/js/2.d5bd1c2e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/expenses-tracker/static/js/2.d5bd1c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e17fa4977c04eb7f3474",
+    "url": "/expenses-tracker/static/js/main.3400d1da.chunk.js"
+  },
+  {
+    "revision": "aee0dd88fd82f6f26996",
+    "url": "/expenses-tracker/static/js/runtime-main.8f6ee18e.js"
+  }
+]);
